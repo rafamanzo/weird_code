f1c2f47f4809813a9962b569143ef604663ddacd
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-04-27 14:24:06 -0300

    weiderer

--- app/main.rb
+++ app/main.rb
@@ -2,17 +2,17 @@ class Foo
   def show
     puts "foo"
   end
-end
 
-class Bar
-  def show
-    puts "bar"
+  class Bar
+    def show
+      puts "bar"
+    end
   end
 end
 
 class All
   def show
     Foo.new.show
-    Bar.new.show
+    Foo::Bar.new.show
   end
 end
